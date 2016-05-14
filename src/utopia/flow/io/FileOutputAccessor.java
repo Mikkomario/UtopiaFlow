460a38f26a32555108e5865eaf15ea0b0134ff1a
Mikkomario <mikko_hilpinen@hotmail.com>
2016-05-14 21:41:50 +0300

    Added some utility methods to file handling

--- src/utopia/flow/io/FileOutputAccessor.java
+++ src/utopia/flow/io/FileOutputAccessor.java
@@ -14,6 +14,7 @@ import utopia.flow.structure.TreeNode;
  * 
  * @author Mikko Hilpinen
  * @since 22.11.2014
+ * @deprecated Replaced with {@link FileUtils}
  */
 public class FileOutputAccessor
 {
