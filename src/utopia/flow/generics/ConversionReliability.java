0b594573c489daad9f9662f96c08b0500b84793b
Mikkomario <mikko_hilpinen@hotmail.com>
2017-12-03 13:55:52 +0200

    Slightly modified the conversion reliability values

--- src/utopia/flow/generics/ConversionReliability.java
+++ src/utopia/flow/generics/ConversionReliability.java
@@ -11,32 +11,32 @@ public enum ConversionReliability
 	 * The data type doesn't have to be casted at all, and already represents the target type. 
 	 * A cast from an integer to a number would be this kind of operation.
 	 */
-	NO_CONVERSION(1),
+	NO_CONVERSION(5),
 	/**
 	 * The data type cast will never fail and preserves the value so that when the value 
 	 * is cast back to the original data type, the value would stay equal. A conversion 
 	 * from an integer to a double number would be a perfect conversion (1 -> 1.0).
 	 */
-	PERFECT(5),
+	PERFECT(11),
 	/**
 	 * The data type cast will never fail, but the value may lose some of its data. The 
 	 * remaining data preserves its meaning and will work properly, however.
 	 * A conversion from a double number to an integer would be a reliable conversion 
 	 * (1.23 -> 1).
 	 */
-	DATA_LOSS(26),
+	DATA_LOSS(23),
 	/**
 	 * The data type cast will never fail, but the meaning of the data may be lost. A conversion 
 	 * from a String representing an integer 2015 to boolean would be this kind of conversion 
 	 * ("2015" -> false)
 	 */
-	MEANING_LOSS(100),
+	MEANING_LOSS(47),
 	/**
 	 * The data type cast may fail, depending from the casted value. The meaning of the 
 	 * value may also be lost. A conversion from a String to a double would be this kind 
 	 * of conversion conversion ("aaa" -> ).
 	 */
-	DANGEROUS(154);
+	DANGEROUS(56);
 	
 	
 	// ATTRIBUTES	-----------------
