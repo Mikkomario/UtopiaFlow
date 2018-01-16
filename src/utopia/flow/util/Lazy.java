3d906fbcf683d2ff514929fc477abead9400bdf9
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2018-01-16 15:42:58 +0200

    Added a new class for lazy item initialization

---
+++ src/utopia/flow/util/Lazy.java
@@ -0,0 +1,50 @@
+package utopia.flow.util;
+
+import java.util.function.Supplier;
+
+/**
+ * This utility class allows lazy initialization of values
+ * @author Mikko Hilpinen
+ * @param <T> The type of object accessed through this class
+ * @since 16.1.2018
+ */
+public class Lazy<T>
+{
+	// ATTRIBUTES	------------------
+	
+	private Option<T> item = Option.none();
+	private Supplier<? extends T> generator;
+	
+	
+	// CONSTRUCTOR	-----------------
+	
+	/**
+	 * Creates a new lazily initialized item
+	 * @param make The function that initializes the item when it is first requested
+	 */
+	public Lazy(Supplier<? extends T> make)
+	{
+		this.generator = make;
+	}
+
+	
+	// OTHER METHODS	------------
+	
+	/**
+	 * @return The lazily initialized item
+	 */
+	public T get()
+	{
+		if (this.item.isDefined())
+			return this.item.get();
+		else
+		{
+			T newItem = this.generator.get();
+			this.item = Option.some(newItem);
+			// Forgets the generator to release memory
+			this.generator = null;
+			
+			return newItem;
+		}
+	}
+}
