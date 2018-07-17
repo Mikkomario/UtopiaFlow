534030b8054dc5b72c5da1dabdbaef62f0a3db3b
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2018-07-17 14:47:41 +0300

    Added some new constructors to optional comparator

--- src/utopia/flow/structure/OptionalComparator.java
+++ src/utopia/flow/structure/OptionalComparator.java
@@ -1,6 +1,8 @@
 package utopia.flow.structure;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.function.Function;
 
 /**
  * This comparator provides an optional result which allows one to chain the comparators together
@@ -11,6 +13,112 @@ import java.util.Comparator;
 @FunctionalInterface
 public interface OptionalComparator<T> extends Comparator<T>
 {
+	// CONSTRUCTOR	-------------------------
+	
+	/**
+	 * Wraps a normal comparator to an optional comparator
+	 * @param c A normal comparator
+	 * @return An optional comparator
+	 */
+	public static <T> OptionalComparator<T> wrap(Comparator<? super T> c)
+	{
+		return (a, b) -> 
+		{
+			int result = c.compare(a, b);
+			if (result == 0)
+				return Option.none();
+			else
+				return Option.some(result > 0);
+		};
+	}
+	
+	/**
+	 * Creates a new comparator that uses mapped values
+	 * @param f a mapping function
+	 * @return An optional comparator based on the map comparison
+	 */
+	public static <T, B extends Comparable<? super B>> OptionalComparator<T> compareBy(Function<? super T, ? extends B> f)
+	{
+		return wrap((a, b) -> f.apply(a).compareTo(f.apply(b)));
+	}
+	
+	/**
+	 * Creates a new comparator that uses mapped values
+	 * @param f a mapping function
+	 * @param comparator A comparator used for comparing the mapped values
+	 * @return An optional comparator based on the map comparison
+	 */
+	public static <T, B> OptionalComparator<T> compareBy(Function<? super T, ? extends B> f, 
+			OptionalComparator<? super B> comparator)
+	{
+		return (a, b) -> comparator.testIsLarger(f.apply(a), f.apply(b));
+	}
+	
+	/**
+	 * @return An optional comparator that can be used for already comparable items
+	 */
+	public static <T extends Comparable<? super T>> OptionalComparator<T> forComparables()
+	{
+		return wrap((a, b) -> a.compareTo(b));
+	}
+	
+	/**
+	 * @return A comparator to be used with collections consisting of comparable items
+	 */
+	public static <T extends Comparable<? super T>, C extends Iterable<? extends T>> OptionalComparator<C> forComparableCollections()
+	{
+		return (l1, l2) -> 
+		{
+			Iterator<? extends T> i1 = l1.iterator();
+			Iterator<? extends T> i2 = l2.iterator();
+			
+			// Compares items sequentially
+			while (i1.hasNext() && i2.hasNext())
+			{
+				int comparison = i1.next().compareTo(i2.next());
+				
+				if (comparison > 0)
+					return Option.some(true);
+				else if (comparison < 0)
+					return Option.some(false);
+			}
+			
+			// If all compared items are equal, collection with more items is larger
+			if (i1.hasNext())
+				return Option.some(true);
+			else if (i2.hasNext())
+				return Option.some(false);
+			
+			// If both had the same items, cannot determine which is larger
+			return Option.none();
+		};
+	}
+	
+	/**
+	 * This comparator compares collections by mapping their values into comparable form. Mapping is done only when 
+	 * necessary.
+	 * @param f A mapping function
+	 * @return A comparator for collections that need mapping
+	 */
+	public static <T, B extends Comparable<? super B>, C extends RichIterable<? extends T>> OptionalComparator<C> 
+			mapCompare(Function<? super T, ? extends B> f)
+	{
+		return compareBy(list -> list.view().map(f), forComparableCollections());
+	}
+	
+	/**
+	 * Creates a new comparator that compares items based on a mapping function. The mapping function may return a 
+	 * collection of comparable items, which are then compared separately
+	 * @param f a mapping function
+	 * @return An optional comparator based on the map comparison
+	 */
+	public static <T, B extends Comparable<? super B>, C extends Iterable<? extends B>> OptionalComparator<T> 
+			compareByCollection(Function<? super T, ? extends C> f)
+	{
+		return compareBy(f, forComparableCollections());
+	}
+	
+	
 	// ABSTRACT	-----------------------------
 	
 	/**
@@ -46,23 +154,6 @@ public interface OptionalComparator<T> extends Comparator<T>
 	public default OptionalComparator<T> and(OptionalComparator<? super T> other)
 	{
 		return (a, b) -> testIsLarger(a, b).orElse(() -> other.testIsLarger(a, b));
-	}
-	
-	/**
-	 * Wraps a normal comparator to an optional comparator
-	 * @param c A normal comparator
-	 * @return An optional comparator
-	 */
-	public static <T> OptionalComparator<T> wrap(Comparator<? super T> c)
-	{
-		return (a, b) -> 
-		{
-			int result = c.compare(a, b);
-			if (result == 0)
-				return Option.none();
-			else
-				return Option.some(result > 0);
-		};
 	}
 	
 	/**
