730c5df9b15cbafb005685765109a86fa06470c3
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2020-02-28 14:29:33 +0200

    Made a stricter action queue test

--- test/utopia/java/flow/test/ActionQueueTest.java
+++ test/utopia/java/flow/test/ActionQueueTest.java
@@ -1,12 +1,15 @@
 package utopia.java.flow.test;
 
 import java.time.Duration;
+import java.util.Random;
 
 import utopia.java.flow.async.Completion;
 import utopia.java.flow.async.Promise;
 import utopia.java.flow.async.ActionQueue;
 import utopia.java.flow.structure.ImmutableList;
+import utopia.java.flow.structure.ListBuilder;
 import utopia.java.flow.structure.range.IntRange;
+import utopia.java.flow.util.Test;
 import utopia.java.flow.util.Unit;
 import utopia.java.flow.util.WaitUtils;
 
@@ -20,6 +23,7 @@ public class ActionQueueTest
 	@SuppressWarnings("javadoc")
 	public static void main(String[] args)
 	{
+		/*
 		// Creates the queue
 		ActionQueue queue = new ActionQueue(1);
 		
@@ -36,5 +40,71 @@ public class ActionQueueTest
 		
 		Completion.ofMany(completions).waitFor();
 		System.out.println("All actions completed. Exiting.");
+		*/
+
+		/*
+		// Tests with limited thread availablility
+		implicit val context: ExecutionContext = new ThreadPool("Test", 2, 4).executionContext
+
+		val queue = new ActionQueue(5)
+
+		val genLock = new AnyRef()
+		var generatedItems = 0
+		val maxGenItems = 1000
+		val random = new Random()
+		val completionsBuffer = new VectorBuilder[Future[Unit]]()
+
+		def task() = WaitUtils.wait(100.millis, new AnyRef())
+
+		println("Starting tasks")
+
+		// Starts generating actions
+		while (generatedItems < maxGenItems)
+		{
+			completionsBuffer += queue.push { task() }
+			generatedItems += 1
+			WaitUtils.wait(random.nextInt(10).millis, genLock)
+		}
+
+		// Checks the completions
+		val completions = completionsBuffer.result()
+		assert(completions.size == maxGenItems)
+
+		println("All completions created")
+		println(s"${ completions.count { _.isCompleted } } / $maxGenItems items completed")
+
+		val successes = completions.waitForSuccesses()
+		assert(successes.size == maxGenItems)
+		println("All completed!")
+		 */
+
+		ActionQueue queue = new ActionQueue(5);
+
+		Object genLock = new Object();
+		int generatedItems = 0;
+		int maxGenItems = 1000;
+		Random random = new Random();
+		ListBuilder<Promise<Unit>> completionsBuffer = new ListBuilder<>();
+
+		Runnable task = () -> WaitUtils.wait(Duration.ofMillis(100), new Object());
+
+		System.out.println("Starting tasks");
+
+		while (generatedItems < maxGenItems)
+		{
+			completionsBuffer.add(queue.push(task));
+			generatedItems += 1;
+			WaitUtils.wait(Duration.ofMillis(random.nextInt(10)), genLock);
+		}
+
+		ImmutableList<Promise<Unit>> completions = completionsBuffer.result();
+		Test.checkEquals(completions.size(), maxGenItems);
+
+		System.out.println("All tasks queued");
+
+		ImmutableList<Unit> successes = completions.map(Promise::waitFor);
+		Test.checkEquals(successes.size(), maxGenItems);
+
+		System.out.println("All completed");
 	}
 }
